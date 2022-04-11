958cc9f330f6aceede10051759603748f778c048
Mert Aşan <mert@yediyuz.com>
2022-04-11 15:21:01 +0300

    Update Authorize.php

--- src/Http/Middleware/Authorize.php
+++ src/Http/Middleware/Authorize.php
@@ -4,35 +4,14 @@ declare(strict_types=1);
 
 namespace Llaski\NovaScheduledJobs\Http\Middleware;
 
-use Laravel\Nova\Nova;
-use Llaski\NovaScheduledJobs\NovaScheduledJobsTool;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authorize
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure(\Illuminate\Http\Request):mixed  $next
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function handle($request, $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        $tool = collect(Nova::registeredTools())->first([$this, 'matchesTool']);
-
-        return optional($tool)->authorize($request) ? $next($request) : abort(403);
-    }
-
-    /**
-     * Determine whether this tool belongs to the package.
-     *
-     * @param \Laravel\Nova\Tool $tool
-     *
-     * @return bool
-     */
-    public function matchesTool($tool)
-    {
-        return $tool instanceof NovaScheduledJobsTool;
+        return $next($request);
     }
 }
