321ec32e511305fb781a2f93689a7f2a7590b148
Mert Aşan <mert@yediyuz.com>
2022-04-11 15:03:10 +0300

    mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,9 @@
 let mix = require('laravel-mix')
+let path = require('path')
 
-mix.js('resources/js/app.js', 'dist/js')
+require('./nova.mix')
+
+mix.setPublicPath('dist')
+    .js('resources/js/app.js', 'js')
+    .vue({ version: 3 })
+    .nova('llaski/nova-scheduled-jobs');
